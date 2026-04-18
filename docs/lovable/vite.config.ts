f66cafab7e676f652f96c44214d1df3bdc826566
Tarik Ruiz <tariklruiz@gmail.com>
2026-04-17 22:50:42 -0300

    update base path to / for fiiguia.com.br

--- docs/lovable/vite.config.ts
+++ docs/lovable/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/b3/lovable/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
