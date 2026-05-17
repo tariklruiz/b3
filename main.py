87f278deeb646c8472965e728808ab013d663f06
Tarik Ruiz <tariklruiz@gmail.com>
2026-05-16 21:52:59 -0300

    fix(cors): allow http://localhost:4173 origin for puppeteer pre-render server

--- main.py
+++ main.py
@@ -98,6 +98,7 @@ app.add_middleware(
         "https://www.fiiguia.com.br",
         "https://tariklruiz.github.io",
         "http://localhost:3000",
+        "http://localhost:4173",
         "http://localhost:5173",
         "http://localhost:8080",
     ],
